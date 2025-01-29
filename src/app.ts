f18c663e015f9d85df53355fe97b04910df9abbc
tos <stevann.tosic@gmail.com>
2025-01-29 11:42:10 +0100

    add form validation

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,35 @@
-function Autobind(_: any, _2: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true;
+
+    if (validatableInput.required) {
+        isValid &&= validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        isValid &&= validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isValid &&= validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid &&= validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid &&= validatableInput.value <= validatableInput.max;
+    }
+
+    return isValid;
+}
+
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor): PropertyDescriptor {
     const originalMethod = descriptor.value;
     return {
         configurable: true,
@@ -32,13 +63,58 @@ class ProjectInput {
         this.attach();
     }
 
-    @Autobind
-    private submitHandler(event: Event) {
-        event.preventDefault();
-        console.log(this.titleInputElement.value);
+    private getUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+        };
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        };
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+
+        // console.log(!validate(titleValidatable) , !validate(descriptionValidatable) , !validate(peopleValidatable))
+        if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+            alert('Not valid input!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
     }
 
-    private attach(){
+    @AutoBind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.getUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+
+            console.log(title)
+            console.log(description)
+            console.log(people)
+
+            this.cleanInputs();
+        }
+    }
+
+    private cleanInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 
